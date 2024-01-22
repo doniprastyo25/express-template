ffbfffda0a8d7bcdb8fd3f76e06ca886db3e406d
doniprastyo1999 <doniprastyo1999@gmail.com>
2024-01-22 21:08:47 +0700

    example generate module

---
+++ src/modules/moduleExample/routes/moduleExampleRoute.js
@@ -0,0 +1,9 @@
+import express from 'express'
+const router = express.Router();
+const authenticateToken = require('../../../settings/middleware/token');
+
+// write route in here
+
+
+
+module.exports = router;
--- src/modules/user/routes/userRoute.js
+++ src/modules/user/routes/userRoute.js
@@ -1,11 +1,13 @@
 import express from 'express'
 const router = express.Router();
-const { authProcess, authenticateToken } = require('../../../settings/middleware/token');
+const authenticateToken = require('../../../settings/middleware/token');
 
+// write route in here
 router.post('/login', authProcess);
 
 router.get('/apiProtection', authenticateToken, (req, res) => {
   res.send("You have access to this API");
 });
+
 
 module.exports = router;
